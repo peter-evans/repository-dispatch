176e52d27af115a5144e78ae09faaa2c3876535e
Peter Evans <peter-evans@users.noreply.github.com>
2020-05-04 18:02:51 +0900

    Convert to Typescript

---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import {inspect} from 'util'
+
+async function run(): Promise<void> {
+  try {
+    const inputs = {
+      token: core.getInput('token'),
+      repository: core.getInput('repository'),
+      eventType: core.getInput('event-type'),
+      clientPayload: core.getInput('client-payload')
+    }
+    core.debug(`Inputs: ${inspect(inputs)}`)
+
+    const [owner, repo] = inputs.repository.split('/')
+
+    const octokit = new github.GitHub(inputs.token)
+
+    await octokit.repos.createDispatchEvent({
+      owner: owner,
+      repo: repo,
+      event_type: inputs.eventType,
+      client_payload: JSON.parse(inputs.clientPayload)
+    })
+  } catch (error) {
+    core.debug(inspect(error))
+    core.setFailed(error.message)
+  }
+}
+
+run()
