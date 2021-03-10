a366ef1a200689cffeda99dab77ccb9c7409458a
Joseph Petersen <josephp90@gmail.com>
2021-03-10 12:54:05 +0100

    support multiple repos

--- src/main.ts
+++ src/main.ts
@@ -2,26 +2,66 @@ import * as core from '@actions/core'
 import * as github from '@actions/github'
 import {inspect} from 'util'
 
+type Repo = {
+  owner: string
+  repo: string
+}
+
+const orgOwner = github.context.repo.owner
+
+function splitRepoString(repoStr: string): Repo {
+  const [owner, repo] = repoStr.split('/')
+  if (repo) {
+    return {
+      owner: owner,
+      repo: repo
+    }
+  }
+
+  return {
+    owner: orgOwner,
+    repo: owner
+  }
+}
+
 async function run(): Promise<void> {
   try {
     const inputs = {
       token: core.getInput('token'),
       repository: core.getInput('repository'),
+      repositories: core.getInput('repositories'),
       eventType: core.getInput('event-type'),
       clientPayload: core.getInput('client-payload')
     }
     core.debug(`Inputs: ${inspect(inputs)}`)
 
-    const [owner, repo] = inputs.repository.split('/')
+    const repositories: Repo[] = []
+
+    if (inputs.repositories) {
+      repositories.push(
+        ...inputs.repositories
+          .replace(/\r/g, '')
+          .split('\n')
+          .map(input => splitRepoString(input))
+      )
+    } else if (inputs.repository) {
+      const repo = splitRepoString(inputs.repository)
+      repositories.push(repo)
+    } else {
+      repositories.push(github.context.repo)
+    }
 
     const octokit = github.getOctokit(inputs.token)
 
-    await octokit.repos.createDispatchEvent({
-      owner: owner,
-      repo: repo,
-      event_type: inputs.eventType,
-      client_payload: JSON.parse(inputs.clientPayload)
-    })
+    await Promise.all(
+      repositories.map(async repo => {
+        await octokit.repos.createDispatchEvent({
+          ...repo,
+          event_type: inputs.eventType,
+          client_payload: JSON.parse(inputs.clientPayload)
+        })
+      })
+    )
   } catch (error) {
     core.debug(inspect(error))
     if (error.status == 404) {
