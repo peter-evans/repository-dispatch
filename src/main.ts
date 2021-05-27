f43d2e4f9b1571b139d808eb15f80a238bee94ff
Peter Evans <18365890+peter-evans@users.noreply.github.com>
2021-05-27 11:21:49 +0900

    Update octokit rest methods

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function run(): Promise<void> {
 
     const octokit = github.getOctokit(inputs.token)
 
-    await octokit.repos.createDispatchEvent({
+    await octokit.rest.repos.createDispatchEvent({
       owner: owner,
       repo: repo,
       event_type: inputs.eventType,
